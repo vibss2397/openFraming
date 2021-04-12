d2cd7efba121e961005a1d87fe011e1628e24152
Vidya Akavoor <vidyaap@bu.edu>
2021-04-12 09:38:39 -0400

    Home page and toolbar style update

--- services/web/frontend/js/client.js
+++ services/web/frontend/js/client.js
@@ -2,17 +2,24 @@ let BASE_URL = "http://" + window.location.host + "/api";
 // uncomment below to test on AWS EC2 instance
 // const BASE_URL = "http://ec2-3-90-135-165.compute-1.amazonaws.com/api";
 
+const urlParams = new URLSearchParams(window.location.search);
+
 /* * * * * * */
 /*  BROWSER  */
 /* * * * * * */
 $(document).ready(function() {
+    const step = urlParams.get('step');
+    const id = urlParams.get('id');
 
     $("#step1").load("components/step1.html", function () {
         $.getScript("js/client.js");
         $.getScript("js/step1.js");
     });
 
-    $("#step2").load("components/step2.html");
+    $("#step2").load("components/step2.html", function () {
+        $.getScript("js/client.js");
+        $.getScript("js/step2.js");
+    });
 
     $("#step3").load("components/step3.html");
 
@@ -22,8 +29,13 @@ $(document).ready(function() {
     });
 
     $("#step5").load("components/step5.html",function(){
-        // $.getScript("js/step4.js");
+        $.getScript("js/client.js");
+        $.getScript("js/step5.js");
     });
+
+    if (step !== null) {
+        changeTabs(step);
+    }
 
 
 });
@@ -45,7 +57,18 @@ function getErrorMessage(message) {
     }
 }
 
+function cleanTextboxInput(textbox_in) {
+    let values = textbox_in.split(',');
+    values = values.map((cat) => {return cat.trim()});
+    return values;
+}
 
-/* * * * * * * */
-/*  HIDE/SHOW  */
-/* * * * * * * */
+function changeTabs(step) {
+    $(`#step1li`).removeClass('active');
+    $(`#step1a`).removeClass('active');
+    $(`#step${step}li`).addClass('active');
+    $(`#step${step}a`).addClass('active');
+
+    $(`#step1`).removeClass('active').removeClass('show');
+    $(`#step${step}`).addClass('active').addClass('show');
+}
