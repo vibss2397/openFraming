dec7cdf3b788381a4cd8d45d31b44f2b0e43fb6f
Vidya Akavoor <vidyaap@bu.edu>
2021-06-24 17:14:57 -0400

    Tweaks from meeting 6/24

--- services/web/frontend/js/client.js
+++ services/web/frontend/js/client.js
@@ -1,4 +1,4 @@
-let BASE_URL = "http://" + window.location.host + "/api";
+let BASE_URL = "http://" + window.location.host + ":5000/api";
 // uncomment below to test on AWS EC2 instance
 // const BASE_URL = "http://ec2-3-90-135-165.compute-1.amazonaws.com/api";
 
